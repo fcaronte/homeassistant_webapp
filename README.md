b75021d0b8a74000714360320d188e3318e38a05
fcaronte <fra.caronte@gmail.com>
2025-01-23 00:14:37 +0100

    Add possibility to edit url directly in-app

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Home Assistant WebView App
 
 Questa app è progettata per aprire una pagina di Home Assistant senza dover usare l'app completa. È utile, ad esempio, su un tablet o un carbox, dove potrebbe essere preferibile una soluzione più leggera e diretta.
+PS: Non sono un programmatore e quindi ho usato Gemini per fare la maggior parte del lavoro.
 
 ## Caratteristiche
 
 - **Semplicità**: Carica direttamente la pagina web di Home Assistant.
 - **Swipe to Refresh**: Aggiorna facilmente la pagina con un gesto di swipe.
 - **Supporto Tema Scuro**: Segue le impostazioni di tema del dispositivo.
+- **Modifica URL**: Puoi Modificare l'url quando vuoi dalla shortcut dell'app (pressione prolungata sull'icona).
 
 ## Utilizzo
 
@@ -25,12 +27,14 @@ Questa app è progettata per aprire una pagina di Home Assistant senza dover usa
 # Home Assistant WebView App
 
 This app is designed to open a Home Assistant page without having to use the complete app. It is useful, for example, on a tablet or carbox, where a lighter and more direct solution might be preferred.
+PS: I'm not a developer, so I used Gemini to do most of the coding work.
 
 ## Features
 
 - **Simplicity**: Directly loads the Home Assistant web page.
 - **Swipe to Refresh**: Easily refresh the page with a swipe gesture.
 - **Dark Mode Support**: Follows the device's theme settings.
+- **Modify URL**: You can change the URL anytime using the app shortcut (long press on the icon).
 
 ## Usage
 
@@ -43,8 +47,3 @@ This app is designed to open a Home Assistant page without having to use the com
 
 - Android Studio
 - Android device with internet access
-
-## Permissions
-
-The app requires the following permissions:
-- Internet access
--- app/src/main/java/com/fcaronte/homeassistant/MainActivity.kt
+++ app/src/main/java/com/fcaronte/homeassistant/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.fcaronte.homeassistant
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -13,12 +15,20 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import androidx.appcompat.view.ContextThemeWrapper
 
 class MainActivity : AppCompatActivity() {
     private lateinit var webView: WebView
     private lateinit var swipeRefreshLayout: SwipeRefreshLayout
-    private var doubleBackToExitPressedOnce = false
+    private var isBackPressedOnce = false
+    private val prefsName = "HomeAssistantPrefs"
+    private val prefURL = "url"
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,12 +36,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         webView = findViewById(R.id.webView)
-        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout)
 
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
-        webView.clearCache(true)
+        webView.clearCache(false)
         webView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
+
+        // Imposta il listener per il refresh
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout)
+
+        swipeRefreshLayout.setOnRefreshListener {
+            webView.reload()
+        }
+
+        // Doppio swipe per uscire
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (isBackPressedOnce) {
+                    finish()
+                } else {
+                    isBackPressedOnce = true
+                    Toast.makeText(this@MainActivity, getString(R.string.please_click_back_again_to_exit), Toast.LENGTH_SHORT).show()
+                    Handler(Looper.getMainLooper()).postDelayed({ isBackPressedOnce = false }, 2000)
+                }
+            }
+        })
+
 
         // Imposta il tema dark se il sistema è impostato in modalità dark
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
@@ -48,8 +78,16 @@ class MainActivity : AppCompatActivity() {
                 swipeRefreshLayout.isRefreshing = false
             }
 
-            override fun onReceivedError(view: WebView, request: WebResourceRequest, error: WebResourceError) {
-                Toast.makeText(this@MainActivity, "Error loading page: ${error.description}", Toast.LENGTH_LONG).show()
+            override fun onReceivedError(
+                view: WebView,
+                request: WebResourceRequest,
+                error: WebResourceError
+            ) {
+                Toast.makeText(
+                    this@MainActivity,
+                    getString(R.string.error_loading_page, error.description),
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
 
@@ -57,21 +95,87 @@ class MainActivity : AppCompatActivity() {
             webView.reload()
         }
 
-        // URL di Home Assistant
-        webView.loadUrl("https://putHereYourHomeAssistantURL/")
+        // Carica l'URL salvato e aprilo nel WebView
+        loadSavedUrl()
+
+        handleIntent(intent)
+
     }
 
-    @Deprecated("Deprecated in Java")
-    override fun onBackPressed() {
-        if (doubleBackToExitPressedOnce) {
-            super.onBackPressed()
-            return
+    private fun loadSavedUrl() {
+        val sharedPreferences = getSharedPreferences(prefsName, Context.MODE_PRIVATE)
+        val savedUrl = sharedPreferences.getString(prefURL, null)
+
+        if (savedUrl.isNullOrEmpty()) {
+            findViewById<TextInputLayout>(R.id.textInputLayoutUrl).visibility = View.VISIBLE
+            showUrlInputDialog(null)
+        } else {
+            webView.loadUrl(savedUrl)
+        }
+    }
+
+    private fun showUrlInputDialog(currentUrl: String?) {
+        val dialogView = layoutInflater.inflate(R.layout.url_input_dialog, null)
+        val input =
+            dialogView.findViewById<TextInputEditText>(R.id.editTextUrl)// Popola il campo di input con l'ultimo URL salvato, se esiste
+        if (!currentUrl.isNullOrEmpty()) {
+            input.setText(currentUrl)
+        }
+        val dialogContext = ContextThemeWrapper(this, R.style.RoundedTextInputLayout)
+        val builder = AlertDialog.Builder(dialogContext)
+        builder.setTitle(getString(R.string.inserisci_url_di_home_assistant))
+        builder.setView(dialogView)
+
+        val dialog = builder.create() // Crea il dialog qui
+        dialog.window?.setBackgroundDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.rounded_dialog_background
+            )
+        ) // Impostalo sfondo arrotondato qui
+
+        // Imposta il listener per il pulsante"OK"
+        dialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK") { _, _ ->
+            val url = input.text.toString()
+            if (url.isNotEmpty()) {
+                saveUrl(url)
+                webView.loadUrl(url)
+            } else {
+                Toast.makeText(this, "L'URL non può essere vuoto", Toast.LENGTH_SHORT).show()
+                showUrlInputDialog(url) // Richiama il dialog se l'URL è vuoto
+            }
+            dialog.dismiss() // Chiudi il dialog dopo aver gestito l'input
         }
 
-        this.doubleBackToExitPressedOnce = true
-        Toast.makeText(this, getString(R.string.please_click_back_again_to_exit), Toast.LENGTH_SHORT).show()
+        // Imposta il listener per il pulsante "Annulla"
+        dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Annulla") { _, _ ->
+            dialog.dismiss() // Chiudi il dialog quando l'utente annulla
+            finish() // Chiudi l'app se l'utente annulla
+        }
 
-        Handler(Looper.getMainLooper()).postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
+        dialog.show() // Mostra il dialog qui
+    }
+
+    private fun saveUrl(url: String) {
+        val sharedPreferences = getSharedPreferences(prefsName, Context.MODE_PRIVATE)
+        with(sharedPreferences.edit()) {
+            putString(prefURL, url)
+            apply()
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (intent.action == "android.intent.action.VIEW") {
+            val sharedPreferences = getSharedPreferences(prefsName, Context.MODE_PRIVATE)
+            val savedUrl = sharedPreferences.getString(prefURL, null)
+            showUrlInputDialog(savedUrl)
+        }
     }
 }
 
